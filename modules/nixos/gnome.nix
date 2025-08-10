301106120dc9b90cbd4ea1cb23f51d5a1f79317c
skil <skil@noreply.codeberg.org>
2025-08-10 13:35:35 +0100

    feat(gnome): further gnome customisation

---
+++ modules/nixos/gnome.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options.services.desktopManager.gnome = {
+    extensions = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+    };
+  };
+
+  config = {
+    environment.systemPackages = config.services.desktopManager.gnome.extensions;
+
+    home-manager.sharedModules = lib.singleton {
+      dconf = {
+        enable = lib.mkDefault true;
+        settings = {
+          "org/gnome/shell" = {
+            disable-user-extensions = false;
+            enabled-extensions = map (ext: ext.extensionUuid) config.services.desktopManager.gnome.extensions ++ lib.optional config.stylix.enable "user-theme@gnome-shell-extensions.gcampax.github.com";
+          };
+        };
+      };
+    };
+  };
+}
