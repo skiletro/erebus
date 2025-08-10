f03cc3b59f96b1d58be1d0103725de9392e2913a
skil <skil@noreply.codeberg.org>
2025-08-10 13:33:46 +0100

    feat(zen): add some extensions

--- config/home/programs/zen.nix
+++ config/home/programs/zen.nix
@@ -22,8 +22,33 @@
         DontCheckDefaultBrowser = true;
         NoDefaultBookmarks = true;
         OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        ExtensionSettings = {
+          "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/github-file-icons/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
       };
-      profiles."default" = {}; # TODO: Declare extensions
+      profiles.default = {
+        id = 0;
+        name = "default";
+        settings = {
+          "zen.welcome-screen.seen" = true;
+          "zen.tabs.vertical.right-side" = true;
+          "browser.toolbars.bookmarks.visiblity" = "always";
+        };
+        search.default = "DuckDuckGo";
+      };
     };
 
     stylix.targets.zen-browser.profileNames = lib.singleton "default";
