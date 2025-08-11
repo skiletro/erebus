69d870a49f9b13213a3b4ab19c093c5fc8ddfc30
skil <skil@noreply.codeberg.org>
2025-08-11 12:44:10 +0100

    fix: fix terminal profile using nixos module instead of home manager module

--- config/nixos/profiles/terminal.nix
+++ config/nixos/profiles/terminal.nix
@@ -5,14 +5,18 @@
 }: {
   options.erebus.profiles.terminal.enable = lib.mkEnableOption "terminal applications that are generally wanted on all systems, but aren't required like the `base` profile.";
 
-  config =
-    lib.mkIf config.erebus.profiles.terminal.enable {
+  config = lib.mkIf config.erebus.profiles.terminal.enable {
+    erebus.programs = {
+      fish.enable = true;
+    };
+
+    home-manager.sharedModules = lib.singleton {
       erebus.programs = {
         bat.enable = true;
         beets.enable = true;
         direnv.enable = true;
         eza.enable = true;
-        fish.enable = true;
       };
     };
+  };
 }
