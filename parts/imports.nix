a0c5b21679d46555afa3a23e6eb401b0b80a66f4
skil <skil@noreply.codeberg.org>
2025-08-14 16:39:41 +0100

    feat(flake): add ability to add custom packages to flake outputs

--- parts/imports.nix
+++ parts/imports.nix
@@ -2,6 +2,7 @@
   imports = [
     ../config
     ../modules
+    ../pkgs
 
     inputs.home-manager.flakeModules.home-manager
   ];
---
+++ pkgs/default.nix
@@ -0,0 +1,14 @@
+{lib, ...}: {
+  perSystem = {
+    pkgs,
+    self',
+    ...
+  }: {
+    packages =
+      builtins.readDir ./.
+      |> lib.filterAttrs (_: value: value == "directory")
+      |> lib.mapAttrs (
+        name: _: pkgs.callPackage ./${name} ({inherit (self') sources;} // self'.packages)
+      );
+  };
+}
