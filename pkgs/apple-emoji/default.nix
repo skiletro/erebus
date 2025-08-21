f439679099052eb80edc53bad5b6f73a64346b65
skil <skil@noreply.codeberg.org>
2025-08-21 20:57:33 +0100

    feat(pkg): add apple-emoji font package

---
+++ pkgs/apple-emoji/default.nix
@@ -0,0 +1,21 @@
+{
+  stdenvNoCC,
+  fetchurl,
+  ...
+}:
+stdenvNoCC.mkDerivation (_finalAttrs: {
+  pname = "apple-emoji";
+  version = "18.4";
+
+  src = fetchurl {
+    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v18.4/AppleColorEmoji.ttf";
+    sha256 = "sha256-pP0He9EUN7SUDYzwj0CE4e39SuNZ+SVz7FdmUviF6r0=";
+  };
+
+  phases = ["installPhase"];
+
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    cp -R $src $out/share/fonts/truetype/AppleColorEmoji.ttf
+  '';
+})
