13e6d3bbf014c6cdd73ae29df8199e9ce2b3d150
skil <skil@noreply.codeberg.org>
2025-08-24 21:21:24 +0100

    feat(pkg): add sf mono nerd font package

---
+++ pkgs/liga-sfmono-nerd-font/default.nix
@@ -0,0 +1,23 @@
+{
+  stdenvNoCC,
+  fetchFromGitHub,
+  ...
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "liga-sfmono-nerd-font";
+  version = "dc5a3e6fcc2e16ad476b7be3c3c17c2273b260ea";
+
+  src = fetchFromGitHub {
+    owner = "shaunsingh";
+    repo = "SFMono-Nerd-Font-Ligaturized";
+    rev = finalAttrs.version;
+    hash = "sha256-AYjKrVLISsJWXN6Cj74wXmbJtREkFDYOCRw1t2nVH2w=";
+  };
+
+  phases = ["installPhase"];
+
+  installPhase = ''
+    mkdir -p $out/share/fonts/opentype
+    cp -R $src/*.otf $out/share/fonts/opentype
+  '';
+})
