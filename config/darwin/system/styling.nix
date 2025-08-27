a62647d54e885ebfd00d8106303910991e3861f8
skil <skil@noreply.codeberg.org>
2025-08-27 16:04:44 +0100

    feat(stylix): use stylix wallpaper and fix font settings

--- config/darwin/system/styling.nix
+++ config/darwin/system/styling.nix
@@ -8,5 +8,12 @@
       echo "Setting wallpaper..."
       /usr/bin/osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"${config.stylix.image}\""
     '';
+
+    # Increase the system font size for macOS to compensate for the
+    # HiDPI displays.
+    stylix.fonts.sizes = with lib; {
+      terminal = mkForce 14;
+      applications = mkForce 14;
+    };
   });
 }
