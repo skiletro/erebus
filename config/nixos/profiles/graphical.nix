b4f19761e7da266895dfdcda1bd64910d4042abf
skil <skil@noreply.codeberg.org>
2025-08-27 16:08:24 +0100

    feat(nixos): add fonts config

--- config/nixos/profiles/graphical.nix
+++ config/nixos/profiles/graphical.nix
@@ -6,7 +6,12 @@
   options.erebus.profiles.graphical.enable = lib.mkEnableOption "graphical applications that are generally wanted on non-headless systems";
 
   config = lib.mkIf config.erebus.profiles.graphical.enable {
-    erebus.desktop.gnome.enable = true;
+    erebus = {
+      desktop.gnome.enable = true;
+      system = {
+        fonts.enable = true;
+      };
+    };
 
     home-manager.sharedModules = lib.singleton {
       erebus.programs = {
---
+++ config/nixos/system/fonts.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.erebus.system.fonts.enable = lib.mkEnableOption "Fonts";
+
+  config = lib.mkIf config.erebus.system.fonts.enable {
+    fonts = {
+      fontDir.enable = true;
+
+      packages = with pkgs; [
+        corefonts # Microsoft Fonts
+        vistafonts # More Microsoft Fonts
+        noto-fonts
+        noto-fonts-extra
+        noto-fonts-cjk-sans # Japanese, Korean, Chinese, etc
+        noto-fonts-color-emoji
+      ];
+    };
+  };
+}
