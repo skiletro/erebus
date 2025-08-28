770b3cf092cce480e910846b07f82bd49279f1ff
skil <skil@noreply.codeberg.org>
2025-08-28 23:35:04 +0100

    feat(gaming): add sunshine service

--- config/nixos/profiles/gaming.nix
+++ config/nixos/profiles/gaming.nix
@@ -6,10 +6,16 @@
   options.erebus.profiles.gaming.enable = lib.mkEnableOption "gaming related *stuff*. This includes VR.";
 
   config = lib.mkIf config.erebus.profiles.gaming.enable {
-    erebus.programs = {
-      gamemode.enable = true;
-      steam.enable = true;
-      wivrn.enable = true;
+    erebus = {
+      programs = {
+        gamemode.enable = true;
+        steam.enable = true;
+        wivrn.enable = true;
+      };
+
+      services = {
+        sunshine.enable = true;
+      };
     };
   };
 }
