e1a5cce0d90a6cf7d12a1f5d2bf86a854ea89654
skil <skil@noreply.codeberg.org>
2025-08-28 23:25:34 +0100

    feat(terminal): add misc cli packages

--- config/nixos/profiles/terminal.nix
+++ config/nixos/profiles/terminal.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.erebus.profiles.terminal.enable = lib.mkEnableOption "terminal applications that are generally wanted on all systems, but aren't required like the `base` profile.";
@@ -18,6 +19,24 @@
         helix.enable = true;
         yazi.enable = true;
       };
+
+      home.packages = with pkgs; [
+        alejandra # nix linter
+        du-dust # fancy du
+        fd # find files
+        file # identify files
+        fzf # fuzzy finder
+        jq # json processor
+        just # make file but better
+        libnotify # notifs through scripts
+        ngrok # reverse proxy
+        ouch # cli for compressing and decompressing formats
+        outfieldr # `tldr` client
+        playerctl # control media w/ cli
+        unrar
+        wget
+        wineWowPackages.stable # wine
+      ];
     };
   };
 }
