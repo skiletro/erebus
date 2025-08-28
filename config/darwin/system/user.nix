78872c61417352625271e0ea52db73ddacb8f7e1
skil <skil@noreply.codeberg.org>
2025-08-28 19:24:42 +0100

    fix(darwin): fish now loads 100% of the time

--- config/darwin/system/user.nix
+++ config/darwin/system/user.nix
@@ -20,17 +20,7 @@
       ];
     };
 
-    # set default shell to fish
-    # TODO: Make this less crude. Currently there is no way to run zsh.
-    # Probabyl make it so Ghostty runs fish as a command on startup,
-    # instead of having it embedded in the .zshrc?
-    # programs.zsh = {
-    #   promptInit = let
-    #     fish = lib.getExe pkgs.fish;
-    #   in ''
-    #     [ -x ${fish} ] && SHELL=${fish} exec ${fish}
-    #   '';
-    # };
+    programs.fish.enable = true; # idk why i need it here too, but i do
 
     home-manager = {
       useGlobalPkgs = true;
