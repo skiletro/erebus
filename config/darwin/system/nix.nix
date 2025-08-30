fb12bc42a65d60279115511dc034fa63a5b72b62
skil <skil@noreply.codeberg.org>
2025-08-30 11:57:43 +0100

    feat(darwin): update nix settings

--- config/darwin/system/nix.nix
+++ config/darwin/system/nix.nix
@@ -1,5 +1,19 @@
 {
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
 
-  nix.settings.experimental-features = ["nix-command" "flakes" "pipe-operators"];
+  nix = {
+    channel.enable = false;
+    settings = {
+      experimental-features = ["nix-command" "flakes" "pipe-operators"];
+      trusted-users = ["jamie" "root" "@wheel"]; # Fixes some "cannot connect to socket" issues
+      warn-dirty = false;
+      http-connections = 50;
+      log-lines = 50;
+      builders-use-substitutes = true;
+      accept-flake-config = true;
+    };
+    optimise.automatic = true;
+  };
 }
