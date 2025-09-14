acff722977d1ebfe12322114453f762b36f8918f
skil <skil@noreply.codeberg.org>
2025-09-14 18:30:35 +0100

    feat(nix): update nixos nix settings

--- config/nixos/system/nix.nix
+++ config/nixos/system/nix.nix
@@ -1,4 +1,39 @@
+{ config, ... }:
 {
-  # options.erebus.system.nix.enable = lib.mkEnableOption "Nix options";
-  config.nixpkgs.config.allowUnfree = true;
+  nix = {
+    # set nix path properly
+    nixPath = [
+      "nixos-config=/home/jamie/Projects/erebus/flake.nix"
+      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+    ];
+
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+      trusted-users = [
+        "jamie"
+        "root"
+        "@wheel"
+      ]; # Fixes some "cannot connect to socket" issues
+      warn-dirty = false;
+      http-connections = 50;
+      log-lines = 50;
+      builders-use-substitutes = true;
+      accept-flake-config = true;
+    };
+
+    optimise = {
+      automatic = true;
+      dates = [
+        "03:45"
+        "07:00"
+      ];
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
 }
