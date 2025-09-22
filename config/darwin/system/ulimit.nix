489ee0c8d0cd53d8b080db804241033097784c4a
skil <skil@noreply.codeberg.org>
2025-09-22 08:00:25 +0100

    fix(darwin): move ulimit fix to devshell

--- config/darwin/system/ulimit.nix
+++
@@ -1,9 +0,0 @@
-{
-  # This fixes the "too many files open" error on build.
-  # When bootstrapping the config, you might need to type this manually.
-  environment.interactiveShellInit =
-    # sh
-    ''
-      ulimit -s unlimited
-    '';
-}
--- parts/devshell.nix
+++ parts/devshell.nix
@@ -4,10 +4,19 @@
     {
       devShells.default = pkgs.mkShellNoCC {
         buildInputs = with pkgs; [
+          git
           just
           nh
         ];
-        shellHook = "just -l -u";
+        shellHook =
+          let
+            # TODO: there is probably a *much* nicer way of doing this.
+            darwinFix = if pkgs.stdenvNoCC.hostPlatform.isDarwin then "ulimit -n 10240" else "";
+          in
+          ''
+            just -l -u
+            ${darwinFix}
+          '';
       };
     };
 }
