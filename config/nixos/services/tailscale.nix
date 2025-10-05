5ae8eef8b9888a2b23e3b4f64a170bb663ee999b
skil <skil@noreply.codeberg.org>
2025-10-05 16:25:51 +0100

    feat(tailscale): add trayscale

--- config/nixos/services/tailscale.nix
+++ config/nixos/services/tailscale.nix
@@ -4,5 +4,11 @@
 
   config = lib.mkIf config.erebus.services.tailscale.enable {
     services.tailscale.enable = true;
+
+    home-manager.sharedModules = lib.mkIf config.erebus.profiles.graphical.enable [
+      (userAttrs: {
+        xdg.autostart.entries = userAttrs.config.lib.erebus.autostartEntry "Trayscale" "${lib.getExe config.services.tailscale.package} --hide-window";
+      })
+    ];
   };
 }
