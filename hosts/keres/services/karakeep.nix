327ff8e4cd60384e4462a06ef74f629f4e98855d
skil <skil@noreply.codeberg.org>
2025-10-13 19:21:12 +0100

    fix(keres/karakeep): update nextauth url

--- hosts/keres/services/karakeep.nix
+++ hosts/keres/services/karakeep.nix
@@ -2,6 +2,7 @@
   services.karakeep = {
     enable = true;
     extraEnvironment = {
+      NEXTAUTH_URL = "https://kk.warm.vodka";
       PORT = "3001";
       DISABLE_SIGNUPS = "true";
       DISABLE_NEW_RELEASE_CHECK = "true";
