fc5235cd20962dd742999db5b37721bf0ed7243c
skil <skil@noreply.codeberg.org>
2025-10-13 13:23:06 +0100

    fix(btop): only install rocm on x86_64 systems

--- config/home/programs/btop.nix
+++ config/home/programs/btop.nix
@@ -6,7 +6,7 @@
 }:
 let
   inherit (lib) mkIf;
-  inherit (pkgs.stdenvNoCC.hostPlatform) isLinux;
+  inherit (pkgs.stdenvNoCC.hostPlatform) isLinux isx86_64;
 in
 {
   options.erebus.programs.btop.enable = lib.mkEnableOption "btop";
@@ -14,7 +14,7 @@ in
   config = mkIf config.erebus.programs.btop.enable {
     programs.btop = {
       enable = true;
-      package = mkIf isLinux pkgs.btop-rocm;
+      package = mkIf (isLinux && isx86_64) pkgs.btop-rocm;
       settings = {
         update_ms = 200;
         show_boxes = "proc cpu mem net gpu0 gpu1 gpu2 gpu3 gpu4 gpu5";
