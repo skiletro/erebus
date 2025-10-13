30db1039c9bf08b5cf8864cd5d6d086d15012ca0
skil <skil@noreply.codeberg.org>
2025-10-13 12:56:51 +0100

    feat(keres/caddy): open firewall

--- hosts/keres/services/caddy.nix
+++ hosts/keres/services/caddy.nix
@@ -1,17 +1,27 @@
-{config, ...}: {
+{ config, ... }:
+{
   services.caddy = {
     enable = true;
     virtualHosts = {
       "warm.vodka" = {
-        serverAlises = ["www.warm.vodka" "warm.vodka"];
+        serverAlises = [
+          "www.warm.vodka"
+          "warm.vodka"
+        ];
         extraConfig = ''
+          header Content-Type text/html
           respond "are you a fan of warm vodka?"
         '';
       };
-      
+
       "kk.warm.vodka".extraConfig = ''
         reverse_proxy :${config.services.karakeep.extraEnvironment.PORT}
       '';
     };
   };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 }
