05da2adf9b9122c510dfad53101ad3c0ff9a7b68
skil <skil@noreply.codeberg.org>
2025-10-13 13:25:01 +0100

    feat(devshell): add neovim and lazygit
    I don't want to add helix because it will probably overwrite the git
    version I have installed, so neovim is a good close second.

--- parts/devshell.nix
+++ parts/devshell.nix
@@ -5,12 +5,14 @@
       devShells.default = pkgs.mkShellNoCC {
         buildInputs = with pkgs; [
           git
+          lazygit
           just
           nh
           nvfetcher
           ssh-to-age
           sops
           nixos-rebuild
+          neovim
         ];
         shellHook = ''
           just -l -u
