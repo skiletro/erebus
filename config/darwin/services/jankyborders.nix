b126773bcb1f8fd6fd13a907490be54f878ff22d
skil <skil@noreply.codeberg.org>
2025-10-30 22:43:09 +0000

    fix(jankyborders): fix blacklist not applying

--- config/darwin/services/jankyborders.nix
+++ config/darwin/services/jankyborders.nix
@@ -10,11 +10,15 @@
           style = "round";
           width = "4.0";
           hidpi = true;
-          blacklist = builtins.concatStringsSep "," [
-            "Steam"
-            "FaceTime"
-            "Screen Sharing"
-          ];
+          blacklist =
+            let
+              list = [
+                "Steam"
+                "FaceTime"
+                "Screen Sharing"
+              ];
+            in
+            "\"${builtins.concatStringsSep "," list}\"";
           active_color = lib.mkDefault "0xffffffff"; # so that stylix can override
           inactive_color = lib.mkDefault "0x00000000"; # so that stylix can override
         };
