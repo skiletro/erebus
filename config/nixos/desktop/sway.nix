e04bff91545edca9ac8402457b817553a0b44c67
skil <skil@noreply.codeberg.org>
2025-11-04 19:29:01 +0000

    refactor(sway): impl keybindings abstractions

--- config/nixos/desktop/sway.nix
+++ config/nixos/desktop/sway.nix
@@ -27,7 +27,6 @@
         config = rec {
           modifier = "Mod4"; # Super
           terminal = "ghostty";
-          menu = "dms ipc call spotlight toggle";
 
           startup = map (cmd: { command = cmd; }) [
             "dms run"
@@ -36,60 +35,69 @@
           ];
 
           keybindings =
+            let
+              exec = cmd: "exec ${cmd}";
+              dms = cmd: "exec dms ipc call ${cmd}";
+              ws = num: "workspace number ${toString num}";
+              mws = num: "move container to workspace number ${toString num}; workspace number ${toString num}";
+              pctl = cmd: "exec ${lib.getExe pkgs.playerctl} -p spotify ${cmd}";
+              focus = cmd: "focus ${cmd}";
+              move = cmd: "focus ${cmd}";
+            in
             (lib.mapAttrs' (key: command: lib.nameValuePair "${modifier}+${key}" command) {
-              "Return" = "exec ${terminal}";
-              "Shift+s" = "exec ${lib.getExe pkgs.sway-contrib.grimshot} copy area";
-              "space" = "exec ${menu}";
-              "f" = "exec zen";
-              "e" = "exec nautilus";
+
+              "Return" = exec terminal;
+              "Shift+s" = exec "${lib.getExe pkgs.sway-contrib.grimshot} copy anything";
+              "space" = dms "spotlight toggle";
+              "f" = exec "zen";
+              "e" = exec "nautilus";
 
               "Shift+q" = "kill";
               "Shift+f" = "fullscreen toggle";
-
-              "Left" = "focus left";
-              "Right" = "focus right";
-              "Up" = "focus up";
-              "Down" = "focus down";
-
-              "Shift+Left" = "move left";
-              "Shift+Right" = "move right";
-              "Shift+Up" = "move up";
-              "Shift+Down" = "move down";
-
-              "1" = "workspace number 1";
-              "2" = "workspace number 2";
-              "3" = "workspace number 3";
-              "4" = "workspace number 4";
-              "5" = "workspace number 5";
-              "6" = "workspace number 6";
-              "7" = "workspace number 7";
-              "8" = "workspace number 8";
-              "9" = "workspace number 9";
-
-              "Shift+1" = "move container to workspace number 1; workspace number 1";
-              "Shift+2" = "move container to workspace number 2; workspace number 2";
-              "Shift+3" = "move container to workspace number 3; workspace number 3";
-              "Shift+4" = "move container to workspace number 4; workspace number 4";
-              "Shift+5" = "move container to workspace number 5; workspace number 5";
-              "Shift+6" = "move container to workspace number 6; workspace number 6";
-              "Shift+7" = "move container to workspace number 7; workspace number 7";
-              "Shift+8" = "move container to workspace number 8; workspace number 8";
-              "Shift+9" = "move container to workspace number 9; workspace number 9";
-
               "Shift+space" = "floating toggle";
 
-              # "Alt_L+s" = "layout stacking";
-              # "Alt_L+w" = "layout tabbed";
-              # "Alt_L+e" = "layout toggle split";
+              "p" = dms "processlist toggle";
+              "n" = dms "notifications toggle";
+              "Shift+p" = dms "powermenu toggle";
+
+              "Left" = focus "left";
+              "Right" = focus "right";
+              "Up" = focus "up";
+              "Down" = focus "down";
+
+              "Shift+Left" = move "left";
+              "Shift+Right" = move "right";
+              "Shift+Up" = move "up";
+              "Shift+Down" = move "down";
+
+              "1" = ws 1;
+              "2" = ws 2;
+              "3" = ws 3;
+              "4" = ws 4;
+              "5" = ws 5;
+              "6" = ws 6;
+              "7" = ws 7;
+              "8" = ws 8;
+              "9" = ws 9;
+
+              "Shift+1" = mws 1;
+              "Shift+2" = mws 2;
+              "Shift+3" = mws 3;
+              "Shift+4" = mws 4;
+              "Shift+5" = mws 5;
+              "Shift+6" = mws 6;
+              "Shift+7" = mws 7;
+              "Shift+8" = mws 8;
+              "Shift+9" = mws 9;
             })
             // {
-              "XF86AudioRaiseVolume" = "exec dms ipc call audio increment 2";
-              "XF86AudioLowerVolume" = "exec dms ipc call audio decrement 2";
-              "XF86AudioNext" = "exec ${lib.getExe pkgs.playerctl} next";
-              "XF86AudioPrev" = "exec ${lib.getExe pkgs.playerctl} previous";
-              "XF86AudioPlay" = "exec ${lib.getExe pkgs.playerctl} play-pause";
-              "XF86Launch9" = "exec ${lib.getExe pkgs.playerctl} -p spotify volume 0.02+";
-              "XF86Launch8" = "exec ${lib.getExe pkgs.playerctl} -p spotify volume 0.02-";
+              "XF86AudioRaiseVolume" = dms "audio increment 2";
+              "XF86AudioLowerVolume" = dms "audio decrement 2";
+              "XF86AudioNext" = dms "mpris next";
+              "XF86AudioPrev" = dms "mpris previous";
+              "XF86AudioPlay" = dms "mpris playPause";
+              "XF86Launch9" = pctl "volume 0.02+";
+              "XF86Launch8" = pctl "volume 0.02-";
             };
 
           gaps = {
@@ -115,6 +123,7 @@
         extraConfig = ''
           for_window [class="Godot"] floating enable
           corner_radius 8
+          focus_on_window_activation focus
         '';
       };
 
