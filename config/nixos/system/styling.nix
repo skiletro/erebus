f0cb55bc6f3290ab32c5f0a87bc85a4e5117fdae
skil <git@skilet.ro>
2025-11-24 21:09:47 +0000

    refactor(styling): neaten up style file for nixos

--- config/nixos/system/styling.nix
+++ config/nixos/system/styling.nix
@@ -1,25 +1,31 @@
 {
   config,
   inputs',
+  lib,
   ...
 }:
 {
-  stylix.cursor = {
-    package =
-      with config.lib.stylix.colors.withHashtag;
-      inputs'.cursors.packages.bibata-modern-cursor.override {
-        background_color = base00;
-        outline_color = base06;
-        accent_color = base00;
-      };
-    name = "Bibata-Modern-Custom";
-    size = 24;
+  stylix = {
+    cursor = {
+      package =
+        with config.lib.stylix.colors.withHashtag;
+        inputs'.cursors.packages.bibata-modern-cursor.override {
+          background_color = base00;
+          outline_color = base06;
+          accent_color = base00;
+        };
+      name = "Bibata-Modern-Custom";
+      size = 24;
+    };
+
+    opacity = {
+      applications = 0.75;
+      popups = 0.75;
+      terminal = 0.75;
+    };
+
+    targets.qt.enable = false; # https://github.com/nix-community/stylix/issues/1946
   };
 
-  stylix.targets.qt.enable = false; # https://github.com/nix-community/stylix/issues/1946
-  stylix.opacity = {
-    applications = 0.75;
-    popups = 0.75;
-    terminal = 0.75;
   };
 }
