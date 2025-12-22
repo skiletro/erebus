79e16cd6845f39d3baa2f101f5e5b508e1c5819a
skil <git@skilet.ro>
2025-12-22 17:33:56 +0000

    feat(eos-cli): fix darwin breaking sometimes

--- pkgs/eos-cli/default.nix
+++ pkgs/eos-cli/default.nix
@@ -11,70 +11,69 @@ let
           
         const SYSTEM = (if ($nu.os-info.name | str downcase) == "macos" { "darwin" } else { "os" })
 
-        def nh_sys [goal: string, ...args: string] {
-            nh $SYSTEM $goal -- ...$args
-        }
-
-        def cd-to-flake [] {
-            cd ("${flakeLocation}" | path expand)
+        def exit-if-darwin [] {
+            if $SYSTEM == "darwin" {
+                print $"(ansi purple)> (ansi reset)This command is unavailable on darwin systems."
+                exit 1
+            }
         }
 
         def main []: nothing -> nothing { help main }
 
         # rebuild and switch to the new generation
         export def "main switch" --wrapped [...args: string] {
-            cd-to-flake
             nix fmt
             git add -A 
-            nh_sys switch ...$args
+            nh $SYSTEM switch --use-substitutes -- ...$args
         }
 
         # rebuild and boot into the new generation
         export def "main boot" --wrapped [...args: string] {
-            cd-to-flake
+            exit-if-darwin
             nix fmt
             git add -A
-            nh_sys boot ...$args
+            nh $SYSTEM boot --use-substitutes -- ...$args
         }
 
-        # build the configuration without activating it
+        # build and activate, without adding to bootloader
         export def "main test" --wrapped [...args: string] {
-            cd-to-flake
-            nix fmt
-            git add -A
-            nh_sys test ...$args
+            exit-if-darwin
+            nh $SYSTEM test --use-substitutes --impure -- ...$args
         }
 
         # update flake inputs and nvfetcher sources
-        export def "main update" [input?: string] {
-            cd-to-flake
-            nix flake update ...($input | default []) --refresh
+        export def "main update" [] {
+            nix flake update --refresh
             nvfetcher -c ./pkgs/nvfetcher.toml -o ./pkgs/_sources/
+        }
+
+        # formats the repo, ready for committing
+        export def "main format" [] {
+            git add -A
+            nix fmt
+            git reset
         }
 
         # open a nix repl for the flake
         export def "main repl" [...args: string] {
-            cd-to-flake
             print $"(ansi purple)! (ansi reset):q to quit!"
-            nh_sys repl ...$args
+            nh $SYSTEM repl ...$args
         }
 
         # garbage-collect and optimise the nix store
         export def "main clean" [] {
-            cd-to-flake
             nh clean all -K 1d
             nix store optimise
         }
 
         # verify and repair the nix store
         export def "main repair" [] {
-            cd-to-flake
             nix-store --verify --check-contents --repair
         }
       '';
 in
 writeBashBin "eos" ''
-  cd ~/Projects/erebus
+  cd ${flakeLocation}
   export NH_FLAKE=${flakeLocation}
   exec ${lib.getExe nuScript} "$@"
 ''
