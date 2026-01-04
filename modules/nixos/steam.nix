1c866f9ed83a99bdcd06d731d7404bef6b5dd3cf
skil <git@skilet.ro>
2026-01-04 21:36:21 +0000

    feat(steam): change button-layout logic

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -57,12 +57,12 @@
                 let
                   inherit (attrs.config.dconf.settings."org/gnome/desktop/wm/preferences") button-layout;
                 in
-                if button-layout == "close,minimize,maximize:" then
+                if button-layout == "" then
                   "macos"
                 else if button-layout == ":minimize,maximize,close" then
                   "windows"
                 else
-                  "elementary"; # left side aligned
+                  "adwaita";
               applySteamTheme = pkgs.writeShellScript "applySteamTheme" ''
                 # This file gets copied with read-only permission from the nix store
                 # if it is present, it causes an error when the theme is applied. Delete it.
