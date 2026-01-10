3e55926a2469c6a9842af4ec19a63f08ff7aef84
skil <git@skilet.ro>
2026-01-10 13:56:11 +0000

    feat(vicinae): add some extensions

--- config/home/services/vicinae.nix
+++ config/home/services/vicinae.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  inputs',
+  ...
+}:
 {
   options.erebus.services.vicinae.enable = lib.mkEnableOption "Vicinae";
 
@@ -6,6 +11,13 @@
     programs.vicinae = {
       enable = true;
       systemd.enable = true;
+      extensions = with inputs'.vicinae-extensions.packages; [
+        # keep-sorted start
+        bluetooth
+        # TODO: fuzzy-files (requires goldfish which isnt packaged rn)
+        nix
+        # keep-sorted end
+      ];
     };
   };
 }
--- flake.nix
+++ flake.nix
@@ -115,6 +115,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    vicinae-extensions = {
+      url = "github:vicinaehq/extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
