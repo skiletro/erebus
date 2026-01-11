62016de10db6f8635a9fac77cffa8521163854e5
skil <git@skilet.ro>
2026-01-11 13:33:13 +0000

    refactor(eos-cli): move scripts to separate files

--- pkgs/eos-cli/default.nix
+++ pkgs/eos-cli/default.nix
@@ -1,86 +1,49 @@
-{ pkgs, lib, ... }:
+{
+  stdenvNoCC,
+  nushell,
+  nvfetcher,
+  git,
+  nh,
+  makeWrapper,
+  lib,
+  ...
+}:
 let
-  inherit (pkgs.writers) writeNuBin writeBashBin;
-  flakeLocation = "~/Projects/erebus";
-  nuScript =
-    writeNuBin "eos" # nu
-      ''
-        alias git = ${lib.getExe pkgs.git};
-        alias nh = ${lib.getExe pkgs.nh};
-        alias nvfetcher = ${lib.getExe pkgs.nvfetcher};
-          
-        const SYSTEM = (if ($nu.os-info.name | str downcase) == "macos" { "darwin" } else { "os" })
-
-        def exit-if-darwin [] {
-            if $SYSTEM == "darwin" {
-                print $"(ansi purple)> (ansi reset)This command is unavailable on darwin systems."
-                exit 1
-            }
-        }
-
-        def main []: nothing -> nothing { help main }
-
-        # rebuild and switch to the new generation
-        export def "main switch" --wrapped [...args: string] {
-            nix fmt
-            git add -A 
-            nh $SYSTEM switch -- ...$args
-        }
-
-        # rebuild and boot into the new generation
-        export def "main boot" --wrapped [...args: string] {
-            exit-if-darwin
-            nix fmt
-            git add -A
-            nh $SYSTEM boot -- ...$args
-        }
-
-        # build and activate, without adding to bootloader
-        export def "main test" --wrapped [...args: string] {
-            exit-if-darwin
-            nh $SYSTEM test --impure -- ...$args
-        }
-
-        # build and compare
-        export def "main build" --wrapped [...args: string] {
-            nix fmt
-            git add -A
-            nh $SYSTEM switch -- ...$args
-        }
-
-        # update flake inputs and nvfetcher sources
-        export def "main update" [] {
-            nix flake update --refresh
-            nvfetcher -c ./pkgs/nvfetcher.toml -o ./pkgs/_sources/
-        }
-
-        # formats the repo, ready for committing
-        export def "main format" [] {
-            git add -A
-            nix fmt
-            git reset
-        }
-
-        # open a nix repl for the flake
-        export def "main repl" [...args: string] {
-            print $"(ansi purple)! (ansi reset):q to quit!"
-            nh $SYSTEM repl ...$args
-        }
-
-        # garbage-collect and optimise the nix store
-        export def "main clean" [] {
-            nh clean all -K 1d
-            nix store optimise
-        }
-
-        # verify and repair the nix store
-        export def "main repair" [] {
-            nix-store --verify --check-contents --repair
-        }
-      '';
+  flakeLocation = "/home/jamie/Projects/erebus";
 in
-writeBashBin "eos" ''
-  cd ${flakeLocation}
-  export NH_FLAKE=${flakeLocation}
-  exec ${lib.getExe nuScript} "$@"
-''
+stdenvNoCC.mkDerivation (attrs: {
+  pname = "eos-cli";
+  version = "0.1";
+
+  src = ./.;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  buildInputs = [
+    nushell
+    nvfetcher
+    git
+    nh
+  ];
+
+  NH_FLAKE = "$HOME/Projects/erebus";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    install -m755 $src/eos.nu $out/bin/eos
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/eos \
+      --prefix PATH : ${lib.makeBinPath attrs.buildInputs} \
+      --set NH_FLAKE "${flakeLocation}" \
+      --chdir "${flakeLocation}" \
+      --inherit-argv0
+  '';
+
+  meta.mainProgram = "eos";
+})
